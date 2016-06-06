73b530c9233734d06e763c5c497bc91a46c38131
Vitalij <vitalij@gmx.net>
2016-06-06 14:45:51 +0200

    auto commit

--- bin/commit.sh
+++ bin/commit.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-set -e
-set -u
-
-#export GIT_SSH="~/.ssh/GIT_SSH.squarealot.sh"
-#ssh -F ~/.ssh/squarealot.ssh.config $@
-
-
 cd /c/Work/squarealot.github.io/
 
-export GIT_SSH="ssh -F ~/.ssh/squarealot.ssh.config"
+export GIT_SSH="~/.ssh/GIT_SSH.squarealot.sh"
 
 git add -A
 git commit -m "auto commit"
